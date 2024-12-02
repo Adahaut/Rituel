d7583a5fdbba88569e8168c1882abd54ac89e965
Geoffrey Laurent <114664596+Albators974@users.noreply.github.com>
2024-12-02 17:15:50 +0100

    Humzn/Spirit Scene and Base MainMenu

---
+++ RituelGame/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject _startCanvas;
+    public GameObject _optionCanvas;
+    public GameObject _selectCharacterCanvas;
+    
+    private GameObject lastCanvasOpen;
+    private GameObject actualCanvas;
+
+    public void StartGame()
+    {
+        _startCanvas.SetActive(false);
+        _selectCharacterCanvas.SetActive(true);
+
+        actualCanvas = _selectCharacterCanvas;
+        lastCanvasOpen = _startCanvas;
+    }
+
+    public void Option()
+    {
+        _startCanvas.SetActive(false);
+        _optionCanvas.SetActive(true);
+
+        actualCanvas = _optionCanvas;
+        lastCanvasOpen = _startCanvas;
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        actualCanvas.SetActive(false);
+        lastCanvasOpen.SetActive(true);
+    }
+}
