9ebd2f2c747298c90747d8a5727c66f49c910e71
UnPseudoTotalementNormal <105439360+UnPseudoTotalementNormal@users.noreply.github.com>
2024-12-03 11:39:39 +0100

    Drawing only worldType character
    text for human & sprite for spirit

--- RituelGame/Assets/Scripts/Enigmas/Ouija/OuijaCore.cs
+++ RituelGame/Assets/Scripts/Enigmas/Ouija/OuijaCore.cs
@@ -1,6 +1,7 @@
 using System;
 using AYellowpaper.SerializedCollections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Enigmas.Ouija
 {
@@ -9,7 +10,7 @@ namespace Enigmas.Ouija
         [SerializeField] private EnigmaData enigmaData;
         [field:SerializeField] public WorldType _currentWorld { get; private set; }
         [field:SerializeField] public OuijaData _currentOuijaData { get; private set; }
-        [field:SerializeField] public SerializedDictionary<char, OuijaCharacter> _characterTransforms { get; private set; }
+        [field:SerializeField] public SerializedDictionary<char, OuijaCharacter> _characterObjects { get; private set; }
         public int _currentCharIndex { get; private set; }
 
         private void Awake()
@@ -19,15 +20,18 @@ namespace Enigmas.Ouija
 
         private void DrawCharacters()
         {
-            foreach (char character in _characterTransforms.Keys)
+            foreach (char character in _characterObjects.Keys)
             {
+                OuijaCharacter ouijaCharacter = _characterObjects[character];
                 if (_currentWorld == WorldType.Human)
                 {
-                    
+                    ouijaCharacter._humanCharacter.gameObject.SetActive(true);
+                    ouijaCharacter._spiritCharacter.gameObject.SetActive(false);
                 }
                 else
                 {
-                    
+                    ouijaCharacter._humanCharacter.gameObject.SetActive(false);
+                    ouijaCharacter._spiritCharacter.gameObject.SetActive(true);
                 }
             }
         }
