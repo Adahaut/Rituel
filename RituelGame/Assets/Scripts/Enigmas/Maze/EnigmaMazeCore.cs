8081969a71489686d29b5a5d071310cdcbf7c01d
Kureo524 <kokoshibo524@gmail.com>
2024-12-03 09:56:49 +0100

    maze folder's creation, maze "EnigmaMazeCore" creation

---
+++ RituelGame/Assets/Scripts/Enigmas/Maze/EnigmaMazeCore.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnigmaMazeCore : MonoBehaviour
+{
+    public GameObject _mazeFramePrefab;
+    private int[,] mazeArray;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
