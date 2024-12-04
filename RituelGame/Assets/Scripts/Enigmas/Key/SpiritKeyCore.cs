e7c1b2f04633bab50d11cadec97bfb181141ef12
UnPseudoTotalementNormal <105439360+UnPseudoTotalementNormal@users.noreply.github.com>
2024-12-04 14:35:46 +0100

    Adding key solution in relic spirit

--- RituelGame/Assets/Scripts/Enigmas/Key/SpiritKeyCore.cs
+++ RituelGame/Assets/Scripts/Enigmas/Key/SpiritKeyCore.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using Enum;
+using TMPro;
+using UnityEngine;
 
 namespace Enigmas.Key
 {
@@ -6,9 +9,28 @@ namespace Enigmas.Key
     {
         [field:SerializeField] public KeyEnigmaData _keyEnigmaData { get; private set; }
         
+        [SerializeField] public TextMeshProUGUI _keyValuesText;
+        
         public void SetEnigmaData(KeyEnigmaData enigmaData)
         {
             _keyEnigmaData = enigmaData;
+        }
+
+        private void Awake()
+        {
+            UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            _keyValuesText.text = "";
+            foreach (KeyTurnSide turnSide in _keyEnigmaData._sideTurnList)
+            {
+                string value = "";
+                value = turnSide == KeyTurnSide.Right ? "Droite" : "Gauche";
+                value += "\n";
+                _keyValuesText.text += value;
+            }
         }
     }
 }
