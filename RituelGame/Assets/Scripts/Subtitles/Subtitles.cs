394a72ecafc8216b85c8ba260aac8e234c7feb3a
PandorTaxe <beaumont.nathan0112@gmail.com>
2024-12-05 09:54:05 +0100

    Create Subtitles Scirpt

---
+++ RituelGame/Assets/Scripts/Subtitles/Subtitles.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subtitles : ScriptableObject
+{
+    public List<string> _subtitles;
+    public AudioClip _audioClip;
+    
+    
+}
