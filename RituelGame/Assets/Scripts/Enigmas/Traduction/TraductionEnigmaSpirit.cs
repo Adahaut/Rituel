15462b3f8f4993fc1556ae03e3cb0f43e3bc90ca
Geoffrey Laurent <114664596+Albators974@users.noreply.github.com>
2024-12-06 14:49:45 +0100

    feedBackButton

--- RituelGame/Assets/Scripts/Enigmas/Traduction/TraductionEnigmaSpirit.cs
+++ RituelGame/Assets/Scripts/Enigmas/Traduction/TraductionEnigmaSpirit.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using AYellowpaper.SerializedCollections;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -17,10 +18,15 @@ public class TraductionEnigmaSpirit : MonoBehaviour
 
     public Vector2 _topLeftFirstButtonPos;
 
+    public CanvasGroup enigmaCanvas;
+    public float canvasFadeDuration;
+    
     private int phraseIndex;
     private string actualPhrase;
     private string[] words;
     private int wordCompletionIndex;
+    private int posIndex = 0;
+    private List<GameObject> buttonListe = new List<GameObject>();
 
     private void Start()
     {
@@ -39,17 +45,17 @@ public class TraductionEnigmaSpirit : MonoBehaviour
     {
         int index = 0;
         
-        foreach (string word in words)
+        foreach (var word in _wordEnglishToLatin)
         {
-            if (!string.IsNullOrWhiteSpace(word))
+            if (!string.IsNullOrWhiteSpace(word.Key))
             {
-                GameObject obj = new GameObject(word);
+                GameObject obj = new GameObject(word.Key);
                 obj.transform.SetParent(wordsParent.transform);
 
                 GameObject text = new GameObject("Text");
                 text.transform.SetParent(obj.transform);
                 TextMeshProUGUI textMesh = text.AddComponent<TextMeshProUGUI>();
-                textMesh.text = _wordEnglishToLatin[word];
+                textMesh.text = _wordEnglishToLatin[word.Key];
                 textMesh.fontSize = 24;
                 textMesh.horizontalAlignment = HorizontalAlignmentOptions.Center;
                 textMesh.verticalAlignment = VerticalAlignmentOptions.Middle;
@@ -59,30 +65,90 @@ public class TraductionEnigmaSpirit : MonoBehaviour
                 image.color = Color.black;
                 button.image = image;
 
-                button.onClick.AddListener(() => ChooseAnswer(word));
+                button.onClick.AddListener(() => ChooseAnswer(word.Key, obj));
 
                 obj.transform.localPosition = new Vector3(_topLeftFirstButtonPos.x + (150 * index), _topLeftFirstButtonPos.y, -0f);
                 obj.transform.localScale = Vector3.one;
+                
+                buttonListe.Add(obj);
 
                 index++;
             }
         }
     }
 
-    private void ChooseAnswer(string word)
+    private void ResetButton()
     {
-        if (string.Equals(word, words[wordCompletionIndex]))
+        posIndex = 0;
+        wordCompletionIndex = 0;
+        int index = 0;
+        foreach (GameObject button in buttonListe)
+        {
+            button.transform.DOLocalMove(
+                new Vector3(_topLeftFirstButtonPos.x + (150 * index), _topLeftFirstButtonPos.y, -0f), 1f);
+            button.GetComponent<Button>().interactable = true;
+            index++;
+        }
+    }
+
+    private void CompleteEnigma()
+    {
+        foreach (GameObject button in buttonListe)
+        {
+            button.GetComponent<Button>().interactable = false;
+        }
+        
+        enigmaCanvas.DOFade(0, canvasFadeDuration);
+        enigmaCanvas.interactable = false;
+        enigmaCanvas.blocksRaycasts = false;
+    }
+    
+    private void ChooseAnswer(string word, GameObject button)
+    {
+        string wordLower = word.ToLower();
+        string wordsActualLower = words[wordCompletionIndex].ToLower();
+        
+        if (string.Equals(wordLower, wordsActualLower))
         {
             Debug.Log($"Correct: {word}");
             wordCompletionIndex++;
+
+            button.GetComponent<Button>().interactable = false;
+            
+            Vector3 wordPosition = GetWordPosition(wordCompletionIndex - 1);
+            Vector3 targetPosition = new Vector3(wordPosition.x, wordPosition.y - 50, 0);
+            
+            button.transform.DOMove(new Vector3(targetPosition.x, 300, 0), 1f);
+
+            posIndex++;
+            
             if (wordCompletionIndex == words.Length)
             {
-                Debug.Log("Success! Phrase complete.");
+                CompleteEnigma();
             }
         }
         else
         {
-            Debug.Log("Failed, wrong word selected.");
+            ResetButton();
         }
+    }
+    
+    private Vector3 GetWordPosition(int wordIndex)
+    {
+        TMP_TextInfo textInfo = _phraseToTranslate.textInfo;
+        
+        TMP_WordInfo wordInfo = textInfo.wordInfo[wordIndex];
+
+        Vector3 firstCharPosition = _phraseToTranslate.transform.TransformPoint(
+            textInfo.characterInfo[wordInfo.firstCharacterIndex].bottomLeft
+        );
+
+        Vector3 lastCharPosition = _phraseToTranslate.transform.TransformPoint(
+            textInfo.characterInfo[wordInfo.lastCharacterIndex].topRight
+        );
+
+        Vector3 wordPosition = (firstCharPosition + lastCharPosition) / 2f;
+
+        return wordPosition;
     }
 }
