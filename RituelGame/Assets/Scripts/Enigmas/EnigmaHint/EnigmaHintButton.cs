a8947deedad9155e85c2784a3796900ba0f0537f
UnPseudoTotalementNormal <105439360+UnPseudoTotalementNormal@users.noreply.github.com>
2024-12-06 10:08:25 +0100

    Adding enigma hint panel

---
+++ RituelGame/Assets/Scripts/Enigmas/EnigmaHint/EnigmaHintButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Enigmas.EnigmaHint
+{
+    public class EnigmaHintButton : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private EnigmaData enigmaData;
+        [SerializeField] private WorldType currentWorldType;
+        
+        public static Action<EnigmaData, WorldType> OnHintButtonClicked;
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            OnHintButtonClicked.Invoke(enigmaData, currentWorldType);
+        }
+    }
+}
