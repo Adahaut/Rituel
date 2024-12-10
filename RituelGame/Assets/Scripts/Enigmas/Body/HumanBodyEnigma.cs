aed3feb74ddc3a59f05629a53e539b0210e9a500
Geoffrey Laurent <114664596+Albators974@users.noreply.github.com>
2024-12-10 10:26:17 +0100

    bosdyChange adding Age

--- RituelGame/Assets/Scripts/Enigmas/Body/HumanBodyEnigma.cs
+++ RituelGame/Assets/Scripts/Enigmas/Body/HumanBodyEnigma.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using AYellowpaper.SerializedCollections;
 using TMPro;
@@ -13,16 +14,21 @@ public class HumanBodyEnigma : MonoBehaviour
     public GameObject _bodyParent;
     
     public SerializedDictionary<string, GameObject> _bodyData = new SerializedDictionary<string, GameObject>(10);
+    public List<int> _age;
+    public List<string> _lastName;
     [SerializeField] private LinkCore linkCore;
     [SerializeField] private EnigmaData enigmaData;
     public Canvas _canvasParent;
     public GameObject _buttonToAccessEnigma;
+    private GameObject body;
+    public GameObject _question;
+    public GameObject _answerZone;
     
     private void Start()
     {
         bodyIndex = Random.Range(0, _bodyData.Count);
 
-        GameObject body = Instantiate(_bodyData.ElementAt(bodyIndex).Value, _bodyParent.transform, true);
+        body = Instantiate(_bodyData.ElementAt(bodyIndex).Value, _bodyParent.transform, true);
         body.GetComponentInChildren<TextMeshProUGUI>().text = "";
         body.transform.localPosition = Vector3.zero;
         body.transform.localScale = Vector3.one;
@@ -32,8 +38,12 @@ public class HumanBodyEnigma : MonoBehaviour
     {
         if (string.Equals(_bodyData.ElementAt(bodyIndex).Key, bodyName, StringComparison.OrdinalIgnoreCase))
         {
-            _canvasParent.gameObject.SetActive(false);
-            _buttonToAccessEnigma.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0.5f);
+            _question.SetActive(false);
+            _answerZone.SetActive(false);
+            body.GetComponentInChildren<TextMeshProUGUI>().text =
+                _bodyData.ElementAt(bodyIndex).Key + " " + _lastName[bodyIndex] + " " + _age[bodyIndex];
+            // _canvasParent.gameObject.SetActive(false);
+            // _buttonToAccessEnigma.GetComponent<Image>().color = new Color(1f, 1f, 1f, 0.5f);
             linkCore.AddLink(enigmaData.LinkToAddIfSuccess);
         }
         else
--- RituelGame/Assets/Scripts/Enigmas/Body/SpiritBodyEnigma.cs
+++ RituelGame/Assets/Scripts/Enigmas/Body/SpiritBodyEnigma.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using AYellowpaper.SerializedCollections;
 using TMPro;
@@ -8,6 +10,12 @@ public class SpiritBodyEnigma : MonoBehaviour
 {
     public GameObject AllBodyParent;
     public SerializedDictionary<string, GameObject> _bodyData = new SerializedDictionary<string, GameObject>();
+    public List<String> _lastName;
+    public List<int> _ages;
+    public GameObject _bodyCanvas;
+    public Image _buttonImage;
+    [SerializeField] private LinkCore linkCore;
+    [SerializeField] private EnigmaData enigmaData;
     
     private void Start()
     {
@@ -21,6 +29,21 @@ public class SpiritBodyEnigma : MonoBehaviour
             GameObject child = Instantiate(_bodyData.ElementAt(i).Value, AllBodyParent.transform, true);
             child.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
             child.transform.GetComponentInChildren<TextMeshProUGUI>().text = _bodyData.ElementAt(i).Key;
+            child.transform.GetComponentInChildren<TextMeshProUGUI>().text += " " + _lastName[i];
+        }
+    }
+
+    public void SubmitAnswer(string answer)
+    {
+        if (_ages.Contains(int.Parse(answer)))
+        {
+            linkCore.AddLink(enigmaData.LinkToAddIfSuccess);
+            _bodyCanvas.SetActive(false);
+            _buttonImage.color = Color.gray;
+        }
+        else
+        {
+            linkCore.RemoveLink(enigmaData.LinkToRemoveIfFail);
         }
     }
 }
