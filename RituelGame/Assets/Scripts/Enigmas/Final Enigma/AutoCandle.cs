8a54699d2bd2c9cd1da72b9baae4371f0065ad57
Geoffrey Laurent <114664596+Albators974@users.noreply.github.com>
2024-12-18 11:41:53 +0100

    WIP

--- RituelGame/Assets/Scripts/Enigmas/Final Enigma/AutoCandle.cs
+++ RituelGame/Assets/Scripts/Enigmas/Final Enigma/AutoCandle.cs
@@ -5,10 +5,18 @@ using UnityEngine.UI;
 
 public class AutoCandle : MonoBehaviour
 {
+    public Sprite _unLightCandle;
     public Sprite _lightedCandle;
 
     public void LightCandle()
     {
         gameObject.GetComponent<Image>().sprite = _lightedCandle;
+        gameObject.GetComponent<AnimeCandle>().StartAnime();
+    }
+
+    public void UnLightCandle()
+    {
+        gameObject.GetComponent<AnimeCandle>().StopAnime();
+        gameObject.GetComponent<Image>().sprite = _unLightCandle;
     }
 }
